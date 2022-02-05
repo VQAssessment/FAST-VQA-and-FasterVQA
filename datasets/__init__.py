0ec247cf6b82c9953c42e3f03c9205a92591541d
TimothyHTimothy <2711123841@qq.com>
2022-02-06 01:50:18 +0800

    delete unused files

--- datasets/__init__.py
+++
@@ -1,3 +0,0 @@
-from .inference_dataset import VQAInferenceDataset, get_fragments
-
-__all__ = ['VQAInferenceDataset', 'get_fragments']
--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .backbone import SwinTransformer3D as VQABackbone
-from .head import VQAHead
-from .evaluator import BaseEvaluator
-
-__all__ = ['VQABackbone', 'VQAHead', 'BaseEvaluator']
