ff9103a5aa4c3341c1b959a87a4a48baa6027e90
TimothyHTimothy <2711123841@qq.com>
2022-12-02 15:23:01 +0800

    v3.1.0

--- vqa.py
+++ vqa.py
@@ -7,6 +7,29 @@ import torch
 import numpy as np
 import argparse
 
+def sigmoid_rescale(score, model="FasterVQA"):
+    mean, std = mean_stds[model]
+    x = (score - mean) / std
+    print(f"Inferring with model [{model}]:")
+    score = 1 / (1 + np.exp(-x))
+    return score
+
+mean_stds = {
+    "FasterVQA": (0.14759505, 0.03613452), 
+    "FasterVQA-MS": (0.15218826, 0.03230298),
+    "FasterVQA-MT": (0.14699507, 0.036453716),
+    "FAST-VQA":  (-0.110198185, 0.04178565),
+    "FAST-VQA-M": (0.023889644, 0.030781006), 
+}
+
+opts = {
+    "FasterVQA": "./options/fast/f3dvqa-b.yml", 
+    "FasterVQA-MS": "./options/fast/fastervqa-ms.yml", 
+    "FasterVQA-MT": "./options/fast/fastervqa-mt.yml", 
+    "FAST-VQA": "./options/fast/fast-b.yml", 
+    "FAST-VQA-M": "./options/fast/fast-m.yml", 
+}
+
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
@@ -15,10 +38,11 @@ if __name__ == "__main__":
     ### options/fast/f3dvqa-b.yml
     ### options/fast/fast-b.yml
     ### options/fast/fast-m.yml
+    
     parser.add_argument(
-        "-o", "--opt", type=str, 
-        default="./options/fast/f3dvqa-b.yml", 
-        help="the option file"
+        "-m", "--model", type=str, 
+        default="FasterVQA", 
+        help="model type: can choose between FasterVQA, FasterVQA-MS, FasterVQA-MT, FAST-VQA, FAST-VQA-M",
     )
     
     ## can be your own
@@ -38,9 +62,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     video_reader = decord.VideoReader(args.video_path)
-
-
-    with open(args.opt, "r") as f:
+    
+    opt = opts.get(args.model, opts["FAST-VQA"])
+    with open(opt, "r") as f:
         opt = yaml.safe_load(f)
 
     ### Model Definition
@@ -60,6 +84,8 @@ if __name__ == "__main__":
                                           )
         else:
             sampler = SampleFrames(clip_len = sample_args["clip_len"], num_clips = sample_args["num_clips"])
+        
+        num_clips = sample_args.get("num_clips",1)
         frames = sampler(len(video_reader))
         print("Sampled frames are", frames)
         frame_dict = {idx: video_reader[idx] for idx in np.unique(frames)}
@@ -71,9 +97,10 @@ if __name__ == "__main__":
         sampled_video = get_spatial_fragments(video, **sample_args)
         mean, std = torch.FloatTensor([123.675, 116.28, 103.53]), torch.FloatTensor([58.395, 57.12, 57.375])
         sampled_video = ((sampled_video.permute(1, 2, 3, 0) - mean) / std).permute(3, 0, 1, 2)
-        num_clips = t_data_opt.get("num_clips",1)
+        
         sampled_video = sampled_video.reshape(sampled_video.shape[0], num_clips, -1, *sampled_video.shape[2:]).transpose(0,1)
         vsamples[sample_type] = sampled_video.to(args.device)
-    
+        print(sampled_video.shape)
     result = evaluator(vsamples)
-    print(f"The quality score of the video is {result.mean().item():.5f}.")
+    score = sigmoid_rescale(result.mean().item(), model=args.model)
+    print(f"The quality score of the video (range [0,1]) is {score:.5f}.")
